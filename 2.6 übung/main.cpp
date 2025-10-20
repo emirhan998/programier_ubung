a4870e1103a7a2b009f2101cc4d896faf3a8a7f4
emirhan998 <147706010+emirhan998@users.noreply.github.com>
2025-10-20 14:53:24 +0200

    Add files via upload

---
+++ 2.6 übung/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+double x,base,exponent,s;
+
+cout<<"geben sie eine zahl für x ein";
+cin>>x;
+
+exponent=32;
+x = base;
+
+s = pow (base,exponent);
+
+cout<<"das ergebnis ist = "<<x;
+}
