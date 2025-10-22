968196842274b08781f47d0936f94cbc481af61a
emirhan998 <147706010+emirhan998@users.noreply.github.com>
2025-10-22 12:25:30 +0200

    Update main.cpp
    Korrigierter Script, funktioniert jetzt!

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -9,8 +9,8 @@ int h,m,s,t,w,x;
 
 cout<<"geben sie eine zahl in sekunden ein";
 cin>>x;
-w=x/604800;             // das modulo wird erst benötigt beim nächsten,
-t=(x%604800)/86400;    // 604800s in einem tag das wird hier wiederholt damit man nur das was übrigbleibt berücksichtigt
+w=x/604800;             // das modulo wird erst benÃ¶tigt beim nÃ¤chsten,
+t=(x%604800)/86400;    // 604800s in einem tag das wird hier wiederholt damit man nur das was Ã¼brigbleibt berÃ¼cksichtigt
 h=(x%86400)/3600;     // hier zb ist die eigentliche rechnung die 3600 das 86400 ist nur damit man sich nicht zu viel nimmt
 m=(x%3600)/60;
 s = x % 60;         //verbleibende Sekunden nach den Minuten
@@ -18,46 +18,50 @@ s = x % 60;         //verbleibende Sekunden nach den Minuten
 
 if (w>0){
 cout<<w;
+
 if (w<2){
 cout<<"woche";
 }
 else{
     cout<<"wochen";
-}
-}
+}}
 if (t>0){
 cout<<t;
-}
+
 if(t<2){
 cout<<"tag";
 }
 else{
     cout<<"tage";
-}
-}
+}}
 if (h>0){
 cout<<h;
-}
-if (h<2)
+
+if (h<2){
 cout<<"stunde";
-else
-    cout<<"stunden";
-if (m>0){
-cout<<m;
-}
-if (m<2) {
-    cout<<"minute";
-    else
-        cout<<"minuten";
-}
-
-
-if (s<2){
-cout <<s<<"sekunde";
 }
 else{
-    cout<<s<<"sekunden";
+    cout<<"stunden";
+}}                         // doppelt da ist das } vom if und vom else da so sorge ich dafÃ¼r das dass "stunde" und "stunden" Ã¼berspringt wird wenn der fall vom if nicht zutrifft
+if (m>0){                 // bei 40 sekunden wird nix ausgegeben
+cout<<m;
+
+if (m<2) {
+    cout<<"minute";
+}
+ else{
+        cout<<"minuten";
 }}
+if (s>0){
+cout <<s;
+
+if (s<2){
+cout<<"sekunde";
+
+}
+else{
+    cout<<"sekunden";
+}}}                         //man kann if statements verschachteln heisst das } placement ist wichtig manche bedingungen kann man damit Ã¼berspringen
 
 
 
