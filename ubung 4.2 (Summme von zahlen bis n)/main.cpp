8e9663b48a3fb95789ea9b51b477b6a9cbc771fc
emirhan998 <147706010+emirhan998@users.noreply.github.com>
2025-10-26 17:01:16 +0100

    Add files via upload

---
+++ ubung 4.2 (Summme von zahlen bis n)/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+
+cout<<"geben sie eine zahl fur n ein ";
+cin>>n;
+
+int sum=0;
+
+for(int i = 0; i<=n ;i++){
+
+    sum=sum+i;
+}
+cout<<"ergebnis = "<<sum;
+}
+
